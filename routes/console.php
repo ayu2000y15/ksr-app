3e431f6c533008789909b396117adce2e2de9b6d
ayu2000y15 <ayu2000y15@nasystem.net>
2025-08-20 23:49:23 +0900

    　　日付：2025/08/20 　変更者：野口 変更内容：新規作成

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// ここにバックアップのスケジュールを追記します
+// ここにバックアップのスケジュールを追記
 Schedule::command('backup:run --only-db')->dailyAt('03:00');
 Schedule::command('backup:run')->dailyAt('04:00');
 Schedule::command('backup:clean')->dailyAt('05:00');
