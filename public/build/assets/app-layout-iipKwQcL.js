fb6f4b0f0752d5218d84fcdf9c18768a30acafdf
ayu2000y15 <ayu2000y15@nasystem.net>
2026-05-06 15:49:42 +0900

    　　日付：2026/05/06 　変更者：野口 変更内容：サーバー移行

---
+++ public/build/assets/app-layout-iipKwQcL.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-B88GB4Fm.js";import{A as s}from"./app-sidebar-layout-BHKt-0Td.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
